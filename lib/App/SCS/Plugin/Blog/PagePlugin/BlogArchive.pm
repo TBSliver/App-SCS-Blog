5db3cb358c84bad6b7a82129f32660d9112c8801
Tom Bloor <tom.bloor@googlemail.com>
2014-12-21 21:24:10 +0000

    Finished getting basic blog page generating

--- lib/App/SCS/Plugin/Blog/PagePlugin/BlogArchive.pm
+++ lib/App/SCS/Plugin/Blog/PagePlugin/BlogArchive.pm
@@ -21,9 +21,36 @@ sub filter_content_zoom {
   my ( $self, $zoom ) = @_;
 
   $zoom->collect( $self->_selector, {
-    filter => sub { return $_ },
+    filter => $self->_filter_callback( $self->page ),
     passthrough => 1,
   });
+}
+
+sub _filter_callback {
+  my ( $self, $page ) = @_;
+  sub {
+    my $stream = shift;
+
+    my %args = (
+      min_depth => $self->min_depth,
+      max_depth => $self->max_depth,
+    );
+
+    $stream->select($self->_selector)->repeat_content(
+      $page->children(%args)->latest()->map(
+        sub {
+          my $o = shift;
+          sub {
+            $_->select('.entry.title')->replace_content($o->title)
+              ->select('.entry.subtitle')->replace_content($o->subtitle)
+              ->select('.entry.description')->replace_content($o->description)
+              ->select('.entry.published_at')->replace_content($o->published_at)
+              ->select('.entry.link')->set_attribute(href => $o->path);
+          }
+        }
+      )
+    );
+  };
 }
 
 sub BUILD {
